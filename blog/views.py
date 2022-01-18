425601cd78b6342d10a4db8f96ec94bd04c0e681
Sarthak Bindal <78961306+bindalsarthak03@users.noreply.github.com>
2022-01-18 14:50:05 +0530

    Second Commit

--- blog/views.py
+++ blog/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('<center><h1>This is my blogging site.</h1></center>')
+    return render(request, 'blog/index.html')
--- shop/views.py
+++ shop/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('<center><h1>This is my shopping site</h1></center>')
+    return render(request,'shop/index.html')
