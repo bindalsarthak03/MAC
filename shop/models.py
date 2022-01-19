4ff96e6e811232b34b3987372b995a2cf4b0cab9
Sarthak Bindal <78961306+bindalsarthak03@users.noreply.github.com>
2022-01-19 08:32:48 +0530

    Third Commit

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    product_id = models.AutoField
+    product_name = models.CharField(max_length=60)
+    desc = models.CharField(max_length=400)
+    pub_date = models.DateField()
