49ad578e782ef38791732ddfe2829c1c765d5394
Sarthak Bindal <78961306+bindalsarthak03@users.noreply.github.com>
2022-01-21 11:08:58 +0530

    Categories Added

--- shop/views.py
+++ shop/views.py
@@ -6,9 +6,19 @@ from math import ceil
 # Create your views here.
 
 def index(request):
-    products = Product.objects.all()
-    nSlides = len(products)//4 + ceil((len(products)/4 - len(products)//4))
-    params = {'number_of_slides': nSlides,'range': (1,nSlides), 'product': products}
+     
+     
+    # params = {'number_of_slides': nSlides,'range': (1,nSlides), 'product': products}
+    # allProds = [[products, range(1,nSlides), nSlides],[products, range(1,nSlides), nSlides]]
+    allProds = []
+    catprods = Product.objects.values('category','id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        n=len(prod)
+        nSlides = n//4 + ceil((n/4)-n//4)
+        allProds.append([prod,range(1,nSlides),nSlides])
+    params  = {'allProds':allProds}
     return render(request,'shop/index.html', params)
 
 def about(request):
